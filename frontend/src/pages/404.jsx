35648023a20c91641f5ca07e9d743ebcf0e286d7
BeevieKyu <marie.a.klausen@gmail.com>
2026-05-26 18:47:55 +0200

    FEATURE: 404 page added

---
+++ frontend/src/pages/404.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router";
+import Button from "../components/Button.jsx";
+
+export default function NotFound() {
+    return (
+        <div>
+            <h1>404</h1>
+            <h2>Page Not Found</h2>
+            <p>
+                Sorry, the page you're looking for doesn't exist.
+            </p>
+            <Button>
+                <Link to="/">
+                    Back to Home
+                </Link>
+            </Button>
+        </div>
+    );
+}
